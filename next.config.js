aa4b560d37d5aadbc243995c6b1145f8ff883717
polykoi <lambwithastick@gmail.com>
2024-11-20 01:17:57 +0200

    Fixed s3 remote image pattern in next.config.js (#3)

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'prodigy-commerce-production.*.s3.amazonaws.com',
+        hostname: 'prodigy-commerce-production.s3.*.amazonaws.com',
         pathname: '/store/**',
         port: '',
       },
